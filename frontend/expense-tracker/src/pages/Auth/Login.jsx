3d4f7d224233bac147599f84fea6cf6648887438
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-12-28 11:28:31 +0530

    1 - Building signup page

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
+  const handleLogin = async(e) => {
     e.preventDefault()
 
     if(!validateEmail(email)) {
