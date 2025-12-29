5e37619526600a8c1b46f3f00112be7110f7b464
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2025-12-29 12:04:55 +0530

    1 - creating user model

---
+++ backend/models/User.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    profileImageUrl: {
+        type: String,
+        default: null
+    }
+}, {timestamps: true})
+
+//Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+// Compare Passwords
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+}
+
+export default mongoose.model('User', UserSchema);
