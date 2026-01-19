51da29794169c01827d70e7a031e283b73a561bd
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2026-01-19 20:25:45 +0530

    2 - dashboard part-2 (building recent transaction and CustomPieChart)

--- frontend/expense-tracker/src/components/Dashboard/RecentTransactions.jsx
+++ frontend/expense-tracker/src/components/Dashboard/RecentTransactions.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { LuArrowRight } from 'react-icons/lu'
+import moment from 'moment'
+import TransactionInfoCard from '../Cards/TransactionInfoCard'
 
 const RecentTransactions = ({transactions, onSeeMore}) => {
   return (
@@ -11,7 +13,19 @@ const RecentTransactions = ({transactions, onSeeMore}) => {
             </button>
         </div>
 
-        <div></div>
+        <div className='mt-6'>
+          {transactions?.slice(0, 5)?.map((item) => (
+            <TransactionInfoCard
+              key={item._id}
+              title={item.type == 'expense' ? item.category : item.source}
+              icon={item.icon}
+              date={moment(item.date).format("Do MMM YYYY")}
+              amount={item.amount}
+              type={item.type}
+              hideDeleteBtn
+            />
+          ))}
+        </div>
     </div>
   )
 }
